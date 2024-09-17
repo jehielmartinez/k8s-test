ab940e0f1b9b0d92b360908cb0dfa8cee9c1ba50
Jehiel Martinez <38274012+jehielmartinez@users.noreply.github.com>
2024-09-16 21:13:25 -0600

    Refactor application and nginx configurations

--- src/index.mjs
+++ src/index.mjs
@@ -1,22 +1,14 @@
 import express from 'express'
-import os from 'os'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+const appName = process.env.APP_NAME || 'App'
 
 app.get('/', (req, res) => {
-  const helloMessage = `Hello World from ${os.hostname()}, version 3.0`
-  console.log(helloMessage)
+  const helloMessage = `I am ${appName}: Running on Port ${port}`
   res.send(helloMessage)
 })
 
-app.get("/nginx", async (req, res) => {
-  const url = "http://nginx"
-  const response = await fetch(url)
-  const body = await response.text()
-  res.send(body)
-})
-
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`${appName} listening at http://localhost:${port}`)
 })
